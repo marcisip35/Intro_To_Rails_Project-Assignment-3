b960cd8b7e4160439d347f004ae296dfc4f6bce1
Marc Isip <165348291+marcisip35@users.noreply.github.com>
2026-07-10 03:20:26 +0000

    Added the about page with tables and erd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "breeds#index"
   resources :breeds, only: [:index, :show]
+  get "about", to: "pages#about"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
